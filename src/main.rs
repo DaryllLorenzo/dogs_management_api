820ee5d3f359780b0c9f3fba15d0bcfd9a194d59
DaryllLorenzo <vikingwarrior4444@gmail.com>
2025-12-24 10:50:07 -0500

    Init: hello world example

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+#[tokio::main]
+pub async fn main() {
+    // Build app by creating router
+    let app = axum::Router::new()
+    .route("/",
+        axum::routing::get(|| async {"Hello World"})
+    );
+
+    // Run app using hyper server on http://localhost:3000.
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app)
+        .await
+        .unwrap();
+}
